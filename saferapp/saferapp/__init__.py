cbf7fa4a436036c5a3f19aecbbc52668fb20de44
Michael Kennedy <mikeckennedy@gmail.com>
2020-06-19 11:41:50 -0700

    Added in the secure.py tween.

--- saferapp/saferapp/__init__.py
+++ saferapp/saferapp/__init__.py
@@ -5,10 +5,18 @@ def main(_, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
-        config.include('pyramid_chameleon')
-        config.include('.routes')
-        config.scan()
+        init_includes(config)
+        configure_secure(config)
+
     return config.make_wsgi_app()
 
 
+def init_includes(config):
+    config.include('pyramid_chameleon')
+    config.include('.routes')
+    config.scan()
 
+
+def configure_secure(config):
+    name = '.infrastructure.secure_tween.set_secure_headers'
+    config.add_tween(name)
